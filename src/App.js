335aebde89ab7e41fd579ac633128a5003c51271
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2024-04-06 12:00:44 +0100

    radius added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,76 @@
 
+// import React, { useState } from "react";
+// import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+// import "./App.css"; // Import your custom styles
+// import { Icon } from "leaflet";
+
+// const App = () => {
+//   const [userLocation, setUserLocation] = useState(null);
+
+//   const customIcon = new Icon({
+//     iconUrl: "https://cdn-icons-png.flaticon.com/512/5591/5591266.png",
+//     iconSize: [38, 38],
+//   });
+
+//   // Custom hook to set the map view to user's location
+//   const GoToLocation = ({ coords }) => {
+//     const map = useMap();
+//     map.flyTo(coords, 15, {
+//       duration: 2, // Transition duration in seconds
+//     });
+//     return null;
+//   };
+
+//   const getLocation = () => {
+//     if (!navigator.geolocation) {
+//       alert("Geolocation is not supported by your browser.");
+//       return;
+//     }
+
+//     navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         const { latitude, longitude, accuracy } = position.coords;
+//         setUserLocation({ latitude, longitude, accuracy });
+//       },
+//       (error) => {
+//         alert("Failed to get current location. Please try again later.");
+//         console.error(error);
+//       }
+//     );
+//   };
+
+//   return (
+//     <div className="App">
+//       <div className="get-location-button">
+//         <button className="button" onClick={getLocation}>Get My Location</button>
+//       </div>
+//       <MapContainer center={[0, 0]} zoom={2} scrollWheelZoom={false}>
+//         <TileLayer
+//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         />
+//         {userLocation && (
+//           <Marker position={[userLocation.latitude, userLocation.longitude]} icon={customIcon}>
+//             <Popup>
+//               Your Location <br />
+//               Latitude: {userLocation.latitude.toFixed(6)} <br />
+//               Longitude: {userLocation.longitude.toFixed(6)} <br />
+//               Accuracy: {userLocation.accuracy.toFixed(2)} meters
+//             </Popup>
+//           </Marker>
+//         )}
+//         {userLocation && <GoToLocation coords={[userLocation.latitude, userLocation.longitude]} />}
+//       </MapContainer>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 import React, { useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap, Circle } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./App.css"; // Import your custom styles
 import { Icon } from "leaflet";
@@ -51,14 +121,23 @@ const App = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {userLocation && (
-          <Marker position={[userLocation.latitude, userLocation.longitude]} icon={customIcon}>
-            <Popup>
-              Your Location <br />
-              Latitude: {userLocation.latitude.toFixed(6)} <br />
-              Longitude: {userLocation.longitude.toFixed(6)} <br />
-              Accuracy: {userLocation.accuracy.toFixed(2)} meters
-            </Popup>
-          </Marker>
+          <>
+            <Marker position={[userLocation.latitude, userLocation.longitude]} icon={customIcon}>
+              <Popup>
+                Your Location <br />
+                Latitude: {userLocation.latitude.toFixed(6)} <br />
+                Longitude: {userLocation.longitude.toFixed(6)} <br />
+                Accuracy: {userLocation.accuracy.toFixed(2)} meters
+              </Popup>
+            </Marker>
+            <Circle
+              center={[userLocation.latitude, userLocation.longitude]}
+              radius={userLocation.accuracy * 0.02} // Use accuracy as radius
+              fillColor="red"
+              fillOpacity={0.3}
+              stroke={false}
+            />
+          </>
         )}
         {userLocation && <GoToLocation coords={[userLocation.latitude, userLocation.longitude]} />}
       </MapContainer>
@@ -67,6 +146,4 @@ const App = () => {
 };
 
 export default App;
-
-
 
